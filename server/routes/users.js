352557cb4d077fbcc98404181b44112fad19b359
John Doe <johndoe@example.com>
2022-12-09 19:06:58 +0800

    a"

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,22 +1,36 @@
+const jwt = require('jsonwebtoken');
 var express = require('express');
+const dotenv = require('dotenv');
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 var router = express.Router();
 
-/* GET users listing. */
+function generateAccessToken(username) {
+  return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+}
+
+// check valiity of the token
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// login the client
 router.put('/', function(req, res, next) {
-  let resource = req.query;
-  let phone = resource.phone;
-  let code = resource.code;
+  const resource = req.query;
+  const phone = resource.phone;
+  const code = resource.code;
   // this should use a call to validate whether it is valid
   // debug purposes, only 123456 is recognised
   if(code != '123456') {
     res.status(403).send('invalid code');
     return;
   }
-  res.send('respond with a resource');
+  const token = generateAccessToken({
+    time: Date.now(),
+    phone: phone + Date.now() + 3,
+  });
+  
+  res.send(token);
 });
 
 module.exports = router;
