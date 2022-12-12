0abfb13806d4c1e84736f4dc457b9fce4ffd2e2f
Your Name <you@example.com>
2022-12-12 17:28:27 +0800

    a

---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+
+    render() {
+        return (
+            <div>欢迎使用香蜜至家服务人员管理系统！</div>
+        );
+    }
+}
+
+export default Login;
